b49dd190ab8cd6f1d01bd6ff54f8640807e2cfa7
Morley Weston <118724731+morleyjamesweston@users.noreply.github.com>
2025-07-25 16:38:49 +0800

    Got svx set up

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	let { children } = $props();
+</script>
+
+{@render children()}
+
+<style>
+	:global(h1) {
+		color: red;
+		font-size: 2rem;
+		margin-bottom: 1rem;
+	}
+</style>
